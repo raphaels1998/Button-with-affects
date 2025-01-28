25ded9e7275d07cb1f7a4eeeafc016773ab47faa
raphaels1998 <50716879+raphaels1998@users.noreply.github.com>
2025-01-28 01:32:23 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('app1').addEventListener('click', function() {
-    alert('App 1 clicked!');
-});
-
-
-document.getElementById('app2').addEventListener('click', function() {
-    alert('App 2 clicked!');
-});
